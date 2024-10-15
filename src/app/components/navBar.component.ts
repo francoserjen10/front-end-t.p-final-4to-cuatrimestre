e91d570d143d60ff708beb7d290d35234779e6e5
Franco_Serjen <francoserjen100@gmail.com>
2024-10-15 12:09:26 -0300

    Creación de navBar (work in progress)

---
+++ src/app/components/navBar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navBar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navBar.component.html',
+  styleUrl: './navBar.component.css'
+})
+export class NavBarComponent {
+}
