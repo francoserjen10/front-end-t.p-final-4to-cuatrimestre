ac96ac868039335e4b51d3149afe76b0c7dd7515
Franco_Serjen <francoserjen100@gmail.com>
2024-11-01 21:30:47 -0300

    Aplicación del gráfico, aplicación de estilo y posisionamiento adecuado de forma responsive,

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { NavBarComponent } from '../../core/components/navBar/navBar.component';
 import { SideBarComponent } from '../../core/components/sideBar/side-bar.component';
 import { DarkModeService } from '../../shared/services/dark-mode.service';
+import { CandlestickChartComponent } from '../../shared/candlestick-chart/candlestick-chart.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, SideBarComponent],
+  imports: [NavBarComponent, SideBarComponent, CandlestickChartComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
