377beb1c38837e717d7dc6dc231330ba82e35809
Franco_Serjen <francoserjen100@gmail.com>
2024-11-07 13:33:14 -0300

    Creación de componente

---
+++ src/app/core/components/ticker/stock-ticker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-ticker',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-ticker.component.html',
+  styleUrl: './stock-ticker.component.css'
+})
+export class StockTickerComponent {
+
+}
