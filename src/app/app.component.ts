97114c41384b85ecf1eb6e9322fc4b7b75e03af2
Franco_Serjen <francoserjen100@gmail.com>
2024-11-15 18:02:23 -0300

    Creacion de barra informativa debajo del nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { DarkModeService } from './shared/services/dark-mode.service';
 import { NavBarComponent } from './core/components/navBar/navBar.component';
 import { SideBarComponent } from './core/components/sideBar/side-bar.component';
+import { StockTickerComponent } from './core/components/ticker/stock-ticker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, SideBarComponent],
+  imports: [RouterOutlet, NavBarComponent, SideBarComponent, StockTickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/core/components/ticker/stock-ticker.component.ts
+++ src/app/core/components/ticker/stock-ticker.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock-ticker',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './stock-ticker.component.html',
   styleUrl: './stock-ticker.component.css'
 })
-export class StockTickerComponent {
+export class StockTickerComponent implements OnInit {
 
+  stocks = [
+    { symbol: 'AAPL', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'TSLA', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'JPM', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'ROG.SW', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'SHEL', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'V', price: 1358.90, change: '+13,91 (+1,04%)' },
+    { symbol: 'WMT', price: 1358.90, change: '+13,91 (+1,04%)' }
+  ];
+
+  ngOnInit(): void {
+
+  }
 }
