92c02897c05e09e0e243766c59065c83341fb7a3
Franco_Serjen <francoserjen100@gmail.com>
2024-11-20 17:45:36 -0300

    Eliminacion de grafico y funcionalidades del home porque no va ahi

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,16 +2,15 @@ import { Component, inject } from '@angular/core';
 import { NavBarComponent } from '../../core/components/navBar/navBar.component';
 import { SideBarComponent } from '../../core/components/sideBar/side-bar.component';
 import { DarkModeService } from '../../shared/services/dark-mode.service';
-import { CandlestickChartComponent } from '../../shared/candlestick-chart/candlestick-chart.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, SideBarComponent, CandlestickChartComponent],
+  imports: [NavBarComponent, SideBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  darkModeService = inject(DarkModeService)
+  darkModeService = inject(DarkModeService);
 }
