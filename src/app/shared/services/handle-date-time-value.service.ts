9da6e9657bc29ba326fb94e6e046d744d2870eda
Franco_Serjen <francoserjen100@gmail.com>
2024-11-22 12:51:05 -0300

    Filtro de las cotizaciones por dias y horas habiles de mercado listo

--- src/app/shared/services/handle-date-time-value.service.ts
+++ src/app/shared/services/handle-date-time-value.service.ts
@@ -10,14 +10,30 @@ export class HandleDateTimeValueService {
   constructor() { }
 
   transformDateAndTimeInTimestamp(cotizaciones: ICotizacion[]): AreaData<Time>[] {
-    return cotizaciones.map((data) => {
+    const cotizacionesInBusinessDays = this.filterByWeekdays(cotizaciones);
+    const cotizacionesInBusinesHours = this.filterByMarketHours(cotizacionesInBusinessDays);
+    return cotizacionesInBusinesHours.map((data) => {
       const dateTimeUtc = `${data.fecha}T${data.hora}Z`
-      const timestamp: UTCTimestamp = (Date.parse(dateTimeUtc) / 1000) as UTCTimestamp; // Convertir de milisegundos a segundos
+      const timestamp: UTCTimestamp = (Date.parse(dateTimeUtc) / 1000) as UTCTimestamp;
       const timestampOslo: UTCTimestamp = (timestamp + 3600) as UTCTimestamp;
       return {
         time: timestampOslo,
-        value: data.cotization
+        value: data.cotization,
       };
+    });
+  }
+
+  filterByWeekdays(cotizaciones: ICotizacion[]): ICotizacion[] {
+    return cotizaciones.filter((cotMarket) => {
+      const day = new Date(cotMarket.fecha).getDay();
+      return day >= 0 && day <= 4;
+    });
+  }
+
+  filterByMarketHours(cotizaciones: ICotizacion[]): ICotizacion[] {
+    return cotizaciones.filter((cotMarket) => {
+      const hour = parseInt(cotMarket.hora);
+      return hour >= 8 && hour <= 15;
     });
   }
 }
