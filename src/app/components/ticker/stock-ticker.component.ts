e13454972625cc76fe8022b8abd400a6ffb379ca
Franco_Serjen <francoserjen100@gmail.com>
2024-11-29 20:12:58 -0300

    Estilos y funcionalidadesd e sticker y cotizaciones en tiempo real listas

--- src/app/components/ticker/stock-ticker.component.ts
+++ src/app/components/ticker/stock-ticker.component.ts
@@ -1,26 +1,68 @@
-import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { DbService } from '../../models/services/db.service';
+import { ICotizacion } from '../../models/interfaces/cotizacion';
 
 @Component({
   selector: 'app-stock-ticker',
   standalone: true,
-  imports: [NgFor],
+  imports: [CommonModule],
   templateUrl: './stock-ticker.component.html',
   styleUrl: './stock-ticker.component.css'
 })
 export class StockTickerComponent implements OnInit {
 
-  stocks = [
-    { symbol: 'AAPL', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'TSLA', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'JPM', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'ROG.SW', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'SHEL', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'V', price: 1358.90, change: '+13,91 (+1,04%)' },
-    { symbol: 'WMT', price: 1358.90, change: '+13,91 (+1,04%)' }
-  ];
+  cotizaciones: ICotizacion[] | any = [];
+
+  constructor(private dbService: DbService) { }
 
   ngOnInit(): void {
+    this.getAllCotizaciones();
+  }
 
+  getAllCotizaciones() {
+    this.dbService.getLatestCotizacionesOfBackEnd()
+      .subscribe({
+        next: (value: ICotizacion[]) => {
+          const groupedCotizaciones = this.groupByCompany(value);
+          this.cotizaciones = this.calculateVariability(groupedCotizaciones);
+        },
+        error(err) {
+          console.error('Error al obtener las cotizaciones', err);
+        },
+      });
+  }
+
+  groupByCompany(cotizaciones: ICotizacion[]): { [key: string]: ICotizacion[] } {
+    return cotizaciones.reduce((acc, cotizacion) => {
+      if (!acc[cotizacion.empresa]) {
+        acc[cotizacion.empresa] = [];
+      }
+      acc[cotizacion.empresa].push(cotizacion);
+      return acc;
+    }, {} as { [key: string]: ICotizacion[] });
+  }
+
+  calculateVariability(groupedCotizaciones: any): any[] {
+    const result = [];
+    for (let empresa in groupedCotizaciones) {
+      const cotizacionesEmpresa = groupedCotizaciones[empresa];
+      cotizacionesEmpresa.sort((a: { fecha: string; hora: string; }, b: { fecha: string; hora: string; }) => new Date(b.fecha + 'T' + b.hora).getTime() - new Date(a.fecha + 'T' + a.hora).getTime());
+      const latestCotizacion = cotizacionesEmpresa[0];
+      const previousCotizacion = cotizacionesEmpresa[1];
+      let variabilidad = 0;
+      let variabilidadPercentage = 0;
+      if (previousCotizacion) {
+        const variabilidadValue = latestCotizacion.cotization - previousCotizacion.cotization;
+        variabilidad = variabilidadValue
+        variabilidadPercentage = ((variabilidadValue / previousCotizacion.cotization) * 100);
+      }
+      result.push({
+        empresa: latestCotizacion.empresa,
+        cotizacion: latestCotizacion.cotization,
+        variabilidad: `${variabilidad.toFixed(2)} (${variabilidadPercentage.toFixed(2)}%)`
+      });
+    }
+    return result;
   }
 }
